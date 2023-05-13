dbd04da0b9596521bed643e9eec96c33cd07c331
Jamie <jamie.styx@gmail.com>
2023-05-13 19:04:48 +0900

    created function to populate the k-bit table

--- sandbox/test.cpp
+++ sandbox/test.cpp
@@ -1,6 +1,6 @@
 #define USE_IPV6 false
 #define DEFAULT_PORT "1234"
-#define K_BITS 8 //Message is encrypted in blocks of k bits
+#define K_BITS 256 //Message is encrypted in blocks of k bits
 
 #include <winsock2.h>
 #include <ws2tcpip.h> //required by getaddrinfo() and special constants
@@ -43,19 +43,49 @@ struct {
 // Generate random k-bit number for Initialisation Vector
 int generate_IV() {
     int IV;
-    IV = rand() % (1 << K_BITS);
+    IV = rand() % K_BITS;
     return IV;
+}
+
+
+struct {
+    unsigned char input[K_BITS];
+    unsigned char output[K_BITS];
+} kbit_exchange;
+
+//populate the kbit table
+void initialise_kbit_table() {
+    unsigned char output[K_BITS];
+    int i = 0;
+    while (i < K_BITS) {
+        output[i] = rand();
+        for (int j = 0; j < i; j++) {
+            if (i == 0) break;
+            if (output[j] == output[i]) {
+                //cout << "DUPLICATE FOUND" << endl;
+                output[i] = rand();
+                j = 0;
+            }
+        }
+        kbit_exchange.input[i] = (unsigned char)i;
+        //cout << "input " << i << " is: "<< (unsigned int)kbit_exchange.input[i] << endl;
+        kbit_exchange.output[i] = output[i];
+        //cout << "output " << i << " is: "<< (unsigned int)kbit_exchange.output[i] << endl;
+        //cout << endl;
+        i++;
+    }
 }
 
 
 int main() {
 
+    srand(time(NULL));
     char send_buffer[200];
     char encrypted_message[200];
     int encrypted;
     string temp_str;
     const char *char_array = NULL;
-    int IV = generate_IV();
+
 
     send_buffer[0] = 'h';
     send_buffer[1] = 'e';
@@ -68,7 +98,7 @@ int main() {
     //RSA ENCRYPT
     fill_n(encrypted_message, strlen(encrypted_message), 0);
     for (int i = 0; i < strlen(send_buffer); i++) {
-        if (i == strlen(send_buffer)-1) {
+        if (i == strlen(send_buffer)) {
             strcat(encrypted_message, "\0"); //strip '\n'
             break;
         } else {
@@ -82,8 +112,32 @@ int main() {
 //////////////////////////////////////////////////////////////////////////
 
 
+// 1. Generate random k-bit number and store as IV (c0)
+    int IV = generate_IV();
+
+// 2. Calculate the ciphertext (message xor c0)
+
+
+// 3. Calculate remaining ciphertext for block i (c(i) = m(i) xor c(i-1))
+
+
     cout << "Original message: " << send_buffer << endl;
     cout << "Encrypted message: " << encrypted_message << endl;
+    cout << "The value of IV is: " << IV << endl;
 
+
+    // Split each character into 4-bit blocks
+    for (int i = 0; i < strlen(encrypted_message); i++) {
+        unsigned char c = encrypted_message[i];
+
+        if (c == ' ') continue;
+        int block1 = (c >> 4) & 0x0F; // get the first 4 bits
+        int block2 = c & 0x0F; // get the last 4 bits
+
+        cout << "Character " << c << " is split into blocks " << block1 << " and " << block2 << std::endl;
+    }
+
+
+    initialise_kbit_table();
 }
 
